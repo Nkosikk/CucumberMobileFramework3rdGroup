a4373d55dbc1c8d4fe87c0daafa8bdb1e7cce905
sgwasira <sgwasira@yahoo.com>
2024-11-14 19:22:45 +0200

    new

--- src/test/java/Steps/StepDef.java
+++ src/test/java/Steps/StepDef.java
@@ -8,26 +8,32 @@ public class StepDef {
     public void landing_page() {
 
     }
+
     @And("I click Wi-Fi")
     public void i_click_wi_fi() {
 
     }
+
     @And("I enter the host")
     public void i_enter_the_host() {
 
     }
+
     @And("I enter the Port")
     public void i_enter_the_port() {
 
     }
+
     @When("I click connect")
     public void i_click_connect() {
 
     }
+
     @Then("the error message is displayed")
     public void the_error_message_is_displayed() {
 
     }
+
     @And("I click Ok button")
     public void i_click_ok_button() {
 
